6f549e33cb461e8ad7375f1b95ec642f5fd84d2d
RegEdits-TSC <Torrent@homeology.dev>
2024-08-05 15:33:18 -0400

    Optimized search and added comments

--- src/search.py
+++ src/search.py
@@ -5,14 +5,16 @@ from src.console import console
 
 class Search():
     """
-    Logic for searching
+    Logic for searching files and folders.
     """
     def __init__(self, config):
         self.config = config
         pass
 
-
     async def searchFile(self, filename):
+        """
+        Search for files with names matching the given filename.
+        """
         os_info = platform.platform()
         filename = filename.lower()
         files_total = []
@@ -21,7 +23,11 @@ class Search():
             return
         file_found = False
         words = filename.split()
+
         async def search_file(search_dir):
+            """
+            Helper function to search for files within a directory.
+            """
             files_total_search = []
             console.print(f"Searching {search_dir}")
             for root, dirs, files in os.walk(search_dir, topdown=False):
@@ -31,11 +37,12 @@ class Search():
                         os_info = platform.platform()
                         if await self.file_search(l_name, words):
                             file_found = True
-                            if('Windows' in os_info):
-                                files_total_search.append(root+'\\'+name)
+                            if 'Windows' in os_info:
+                                files_total_search.append(root + '\\' + name)
                             else:
-                                files_total_search.append(root+'/'+name)
+                                files_total_search.append(root + '/' + name)
             return files_total_search
+
         config_dir = self.config['DISCORD']['search_dir']
         if isinstance(config_dir, list):
             for each in config_dir:
@@ -46,6 +53,9 @@ class Search():
         return files_total
 
     async def searchFolder(self, foldername):
+        """
+        Search for folders with names matching the given foldername.
+        """
         os_info = platform.platform()
         foldername = foldername.lower()
         folders_total = []
@@ -54,36 +64,38 @@ class Search():
             return
         folders_found = False
         words = foldername.split()
+
         async def search_dir(search_dir):
+            """
+            Helper function to search for folders within a directory.
+            """
             console.print(f"Searching {search_dir}")
             folders_total_search = []
             for root, dirs, files in os.walk(search_dir, topdown=False):
-
                 for name in dirs:
                     l_name = name.lower()
-
                     os_info = platform.platform()
-
                     if await self.file_search(l_name, words):
-                        folder_found = True
-                        if('Windows' in os_info):
-                            folders_total_search.append(root+'\\'+name)
+                        folders_found = True
+                        if 'Windows' in os_info:
+                            folders_total_search.append(root + '\\' + name)
                         else:
-                            folders_total_search.append(root+'/'+name)
-            
+                            folders_total_search.append(root + '/' + name)
             return folders_total_search
+
         config_dir = self.config['DISCORD']['search_dir']
         if isinstance(config_dir, list):
             for each in config_dir:
                 folders = await search_dir(each)
-                
                 folders_total = folders_total + folders
         else:
             folders_total = await search_dir(config_dir)
         return folders_total
 
-        return folders_total
     async def file_search(self, name, name_words):
+        """
+        Check if all words in name_words are present in the name.
+        """
         check = True
         for word in name_words:
             if word not in name:
