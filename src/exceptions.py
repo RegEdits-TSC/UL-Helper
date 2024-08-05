019b88c96a7ac240684dc890e41275e3e859f1c2
RegEdits-TSC <Torrent@homeology.dev>
2024-08-05 15:30:05 -0400

    Added comments and applied better logic

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,33 +1,47 @@
 class LoginException(Exception):
+    """
+    Exception raised for errors that occur during login operations.
+    """
     def __init__(self, *args, **kwargs):
-        default_message = 'An error occured while logging in'
-        # if any arguments are passed...
-        # If you inherit from the exception that takes message as a keyword
-        # maybe you will need to check kwargs here
+        default_message = 'An error occurred while logging in'
+        # If any arguments are passed, use them
         if args:
-            # ... pass them to the super constructor
             super().__init__(*args, **kwargs)
-        else: # else, the exception was raised without arguments ...
-                 # ... pass the default message to the super constructor
-                 super().__init__(default_message, **kwargs)
+        else:
+            # Otherwise, use the default message
+            super().__init__(default_message, **kwargs)
+
 
 class UploadException(Exception):
+    """
+    Exception raised for errors that occur during upload operations.
+    """
     def __init__(self, *args, **kwargs):
-        default_message = 'An error occured while uploading'
-        # if any arguments are passed...
-        # If you inherit from the exception that takes message as a keyword
-        # maybe you will need to check kwargs here
+        default_message = 'An error occurred while uploading'
+        # If any arguments are passed, use them
         if args:
-            # ... pass them to the super constructor
             super().__init__(*args, **kwargs)
-        else: # else, the exception was raised without arguments ...
-                 # ... pass the default message to the super constructor
-                 super().__init__(default_message, **kwargs)
+        else:
+            # Otherwise, use the default message
+            super().__init__(default_message, **kwargs)
 
 
 class XEMNotFound(Exception):
+    """
+    Exception raised when an XEM (extra episode metadata) is not found.
+    """
     pass
+
+
 class WeirdSystem(Exception):
+    """
+    Exception raised for unexpected or unsupported system configurations.
+    """
     pass
+
+
 class ManualDateException(Exception):
+    """
+    Exception raised for issues related to manually specified dates.
+    """
     pass
